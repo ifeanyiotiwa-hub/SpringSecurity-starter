ff9a247af5d233d7b0e1a6b919cf4998f5914c6a
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-02-13 10:13:56 +0100

    creates MVCConfig

---
+++ src/main/java/org/regent/securitypractice/config/MvcConfig.java
@@ -0,0 +1,20 @@
+package org.regent.securitypractice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
+/*
+ This web Application is based on SpringMVC. As a result, I need to configure spring mvc and set up controllers to expose
+ these templates
+*/
