3d84b9ad01825d04ad8daa01c9d3f9bf16090dc7
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-02-13 09:41:44 +0100

    Project start

---
+++ src/main/java/org/regent/securitypractice/SecurityPractice2Application.java
@@ -0,0 +1,13 @@
+package org.regent.securitypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityPractice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityPractice2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/regent/securitypractice/SecurityPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.regent.securitypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityPractice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
